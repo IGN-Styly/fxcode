527c94755a327776f840a60d4af3f68c4f8143f1
Styly <claudiotorresptpt+git@gmail.com>
2026-08-29 16:16:04 +0100

    get window size

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use termion::terminal_size;
+
+#[derive(Debug, Clone, Copy, Default)]
+struct App {
+    width: u16,
+    height: u16,
+}
+
+impl App {
+    fn init(&mut self) {
+        let (x, y) = terminal_size().unwrap();
+        self.width = x;
+        self.height = y;
+    }
+}
+
+fn main() {
+    let mut app = App::default();
+    app.init();
+    println!("x:{} y:{}", app.width, app.height);
+}
